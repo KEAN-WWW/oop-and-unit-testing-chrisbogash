09264c7d18a85a7bf3f2b18021dfc7e28236371c
chrisbogash <126812173+chrisbogash@users.noreply.github.com>
2025-02-04 17:04:59 -0500

    renamed file to tests_addition

--- tests/test_addition.py
+++
@@ -1,7 +0,0 @@
-"""This is the addition test file"""
-from app.addition import add
-
-
-def test_addition():
-    """ Add the numbers"""
-    assert add(2, 2) == 4
--- tests/tests_addition.py
+++ tests/tests_addition.py
@@ -1,2 +1,7 @@
+"""This is the addition test file"""
+from app.addition import add
+
+
 def test_addition():
-    pass
+    """ Add the numbers"""
+    assert add(2, 2) == 4
